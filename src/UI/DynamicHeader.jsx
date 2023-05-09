368388bc377267124abf9dc90d56e57321dd69ba
Erik Truckner <eriktruckner@comcast.net>
2023-05-09 07:02:22 -0400

    dynamic background

--- src/UI/DynamicHeader.jsx
+++ src/UI/DynamicHeader.jsx
@@ -2,9 +2,10 @@ import '../index.css'
 
 const DyanmicHeader = ({ followingEarth, setFollowingEarth }) => {
   const headerText = followingEarth ? 'Earth System' : 'Solar System'
+  const headerClass = followingEarth ? 'earth-transition' : 'solar-transition'
   return (
     <div className='dynamic-header-flex-container'>
-      <div className='dynamic-header'>
+      <div className={`dynamic-header ${headerClass}`}>
         <h1 className='dynamic-header-h1'>{headerText}</h1>
       </div>
     </div>
