63e475642dbe2396b5e7f49023302da2bbf68372
Erik Truckner <eriktruckner@comcast.net>
2023-05-10 09:04:12 -0400

    loading screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 
 import MainContainer from './MainContainer'
 import DyanmicHeader from './UI/DynamicHeader'
 import CockPit from './UI/CockPit'
 
+const LoadingScreen = () => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: '#000000',
+        color: '#ffffff',
+        fontSize: '16px',
+      }}>
+      <h1>The Robots are building the site...</h1>
+    </div>
+  )
+}
+
 function App() {
   const [followingEarth, setFollowingEarth] = useState(false)
 
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const canvasRef = useRef()
+
+  const handleCreated = () => {
+    setIsLoaded(true)
+  }
+
   return (
     <>
+      {!isLoaded && <LoadingScreen />}
       <Canvas
+        ref={canvasRef}
+        onCreated={handleCreated}
         shadows
         camera={{
           fov: 55,
@@ -19,16 +50,22 @@ function App() {
           position: [16.14, 8.32, 19.81],
         }}>
         <color attach='background' args={['black']} />
+
         <MainContainer
           followingEarth={followingEarth}
           setFollowingEarth={setFollowingEarth}
         />
       </Canvas>
       {/* <CockPit /> */}
-      <DyanmicHeader
-        followingEarth={followingEarth}
-        setFollowingEarth={setFollowingEarth}
-      />
+
+      {isLoaded && (
+        <>
+          <DyanmicHeader
+            followingEarth={followingEarth}
+            setFollowingEarth={setFollowingEarth}
+          />
+        </>
+      )}
     </>
   )
 }
---
+++ src/UI/StartScreen.jsx
@@ -0,0 +1,10 @@
+const StartScreen = () => {
+  return (
+    <div className='start-screen'>
+      <h1>Welcome to the Solar System Experience!</h1>
+      <div>START</div>
+    </div>
+  )
+}
+
+export default StartScreen
