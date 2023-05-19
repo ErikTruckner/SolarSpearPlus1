d82c44194bf5e117cb050a33e094360ee55f3863
Erik Truckner <eriktruckner@comcast.net>
2023-05-19 15:01:27 -0400

    png trans fixed

--- src/UI/EarthButton.jsx
+++ src/UI/EarthButton.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import '../index.css'
 
 import earthTN from '../images/earthTN.png'
-import sunTN from '../images/sunTN.png'
-import sunTN1 from '../images/sunTN1.png'
+import sunTN from '../images/sun123.png'
 
 const EarthButton = ({ followingEarth, setFollowingEarth }) => {
   const toggleFollowingEarth = () => {
     setFollowingEarth((prevFollowingEarth) => !prevFollowingEarth)
   }
   const earthButtonImage = followingEarth ? (
-    <img src={sunTN1} />
+    <img src={sunTN} />
   ) : (
     <img src={earthTN} />
   )
